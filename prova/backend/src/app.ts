d405dc5478cfd41c94e2dcb769761bbb0a12c07b
ladeira1 <joaoladeirag@gmail.com>
2022-04-07 20:40:20 -0300

    Connect to database before initializing

--- prova/backend/src/app.ts
+++ prova/backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { MongoHelper } from './infra/database/mongo-helper';
 import { provaRouter } from './presentation/route/prova.route';
 
 
@@ -6,15 +7,18 @@ const app = express();
 
 app.use(express.json())
 
-app.get('/', (_, res) => {
-    res.json({
-        app: 'prova-server',
-        version: '1.0.0'
+MongoHelper.connect(process.env.DATABASE_URL).then(async () => {
+    app.get('/', (_, res) => {
+        res.json({
+            app: 'prova-server',
+            version: '1.0.0'
+        })
     })
-})
+    
+    app.use('/api/prova', provaRouter)
+    
+    app.listen(3333, () => {
+        console.log('listening at localhost:3333')
+    })
+}).catch(err => console.log('Error connection to database', err))
 
-app.use('/api/prova', provaRouter)
-
-app.listen(3333, () => {
-    console.log('listening on localhost:3333')
-})
