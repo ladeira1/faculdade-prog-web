fdafb3bebfcae38d69e05edadee8fe4a9265abca
ladeira1 <joaoladeirag@gmail.com>
2022-04-07 19:36:19 -0300

    Config backend

--- prova/backend/jest.config.js
+++ prova/backend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: "coverage",
   coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'
