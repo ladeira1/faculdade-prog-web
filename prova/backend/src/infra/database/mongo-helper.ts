942a0352fd47213cf7f3c055421ed046d4499a06
ladeira1 <joaoladeirag@gmail.com>
2022-04-07 20:32:14 -0300

    Create MongoHelper

---
+++ prova/backend/src/infra/database/mongo-helper.ts
@@ -0,0 +1,29 @@
+import { MongoClient, Collection } from "mongodb";
+
+export const MongoHelper = {
+    client: null as MongoClient,
+    url: null as string,
+
+    async connect (uri: string): Promise<void> {
+        this.uri = uri
+        this.client = await MongoClient.connect(uri)
+    },
+
+    async disconnect (): Promise<void> {
+        await this.client.close()
+        this.client = null
+    },
+
+    getCollection (name: string): Collection {
+        return this.client.db().collection(name)
+    },
+
+    map: (data: any): any => {
+        const { _id, ...rest } = data
+        return { ...rest }
+    },
+
+    mapCollection: (collections: any[]): any[] => {
+        return collections.map(collection => MongoHelper.map(collection))
+    }
+}
