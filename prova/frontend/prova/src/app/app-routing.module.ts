0e93aa0976cd6862c0304c1407081ee51e677f83
Aluno <aluno@insg.com.br>
2022-05-19 21:45:20 -0300

    oi

--- prova/frontend/prova/src/app/app-routing.module.ts
+++ prova/frontend/prova/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RouteGuard } from './core/guards/route.guard';
 import { AlunoComponent } from './modules/aluno/aluno.component';
 import { LoginComponent } from './modules/login/login.component';
+
 import { ProvaComponent } from './modules/prova/prova.component';
 
 const routes: Routes = [
