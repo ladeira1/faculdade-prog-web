5e73fcd394723587b13f6ff5594f9433d19f1bf3
Aluno <aluno@insg.com.br>
2022-05-19 21:40:13 -0300

    Aula 3

--- prova/frontend/prova/src/app/app-routing.module.ts
+++ prova/frontend/prova/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlunoComponent } from './modules/aluno/aluno.component';
+import { LoginComponent } from './modules/login/login.component';
+import { ProvaComponent } from './modules/prova/prova.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+  { path: 'prova', component: ProvaComponent },
+  { path: 'aluno', component: AlunoComponent },
+  { path: '**', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- prova/frontend/prova/src/app/app.module.ts
+++ prova/frontend/prova/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/login/login.component';
+import { ProvaComponent } from './modules/prova/prova.component';
+import { AlunoComponent } from './modules/aluno/aluno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProvaComponent,
+    AlunoComponent
   ],
   imports: [
     BrowserModule,
