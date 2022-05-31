81ab03b4983a8bfd9ee15029269baf2c38565551
Joao Ladeira <joaoladeirag@gmail.com>
2022-05-30 21:36:13 -0300

    style: login screen

--- prova/frontend/prova/src/app/app.module.ts
+++ prova/frontend/prova/src/app/app.module.ts
@@ -6,19 +6,12 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './modules/login/login.component';
 import { ProvaComponent } from './modules/prova/prova.component';
 import { AlunoComponent } from './modules/aluno/aluno.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    ProvaComponent,
-    AlunoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginComponent, ProvaComponent, AlunoComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
